3d3e706ae9498c1737e079fbc6877236bcfe71dd
Dimitris Kagialis <kagialisd@gmail.com>
2020-02-28 19:01:50 +0000

    version 1.3

--- earthquakes/earthquakes.py
+++ earthquakes/earthquakes.py
@@ -19,9 +19,9 @@ def main():
     for year in years:
         print year
         eq_list_raw = Acquisition.request(year,magnitudeOver)
-        #eq_list_temp = Preprocessing.cleanHeaders(eq_list_raw)
-        #eq_list = Preprocessing.splitDateTime(eq_list_temp)
-        #Store.toFile(eq_list)
+        eq_list_temp = Preprocessing.cleanHeaders(eq_list_raw)
+        eq_list = Preprocessing.splitDateTime(eq_list_temp)
+        Store.toFile(eq_list,year)
         print "Data acquisition ended"
 
 
--- earthquakes/store.py
+++ earthquakes/store.py
@@ -4,20 +4,20 @@ import os
 class Store:
 
     @classmethod
-    def toFile(cls, eq_list):
+    def toFile(cls, eq_list,year):
         cls.createFolder()
-        cls.createFile()
+        cls.createFile(year)
         count = 0
-        with open('../data/earthquakes.csv', 'a') as writer:
+        with open('../data/earthquakes{}.csv'.format(year), 'a') as writer:
             for eq in eq_list:
                 count = count + 1
                 eq_str = ",".join(eq)
                 writer.write("%s\r\n" % (eq_str))
-            print "Data stored to file, records: ",count
+            print "Single date data stored to file, records: ",count
 
     @classmethod
-    def createFile(cls):
-        with open('../data/earthquakes.csv', 'w') as writer:
+    def createFile(cls,year):
+        with open('../data/earthquakes{}.csv'.format(year), 'w') as writer:
             writer.write("")
 
     @classmethod
